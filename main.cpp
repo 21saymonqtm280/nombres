919864c19bb411aae0b808e53c540a8edf69c45d
SaymonC-MM <90119229+21saymonqtm280@users.noreply.github.com>
2021-10-13 16:20:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// para poder tener acceso a los dispositivos estándar de entrada y/o salida
+using namespace std;
+//para dar espacio a los nombres donde esta la libreria estandar 
+int main(){
+//main es el punto de inicio de la ejecucion 
+cout<< "Saymon\n ";
+
+cout<< "Castillo\n ";
+
+cout<< "Meneses\n ";
+
+cout<< "10-2B\n";
+}
